ffa997f944b566bb2f51fa3170becfc377dc3711
Ken Dacanay <ken.p.r.dacanay@gmail.com>
2020-02-26 06:44:15 -0500

    began javascript code, wrote letterChoices for computer and user, and counter for page

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+//these will be the letter choices for computer and user use
+var letterChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+//this is to set/reset counters 
+var wins=0;
+var losses=0;
+var guesses=9;
+var guessesLeft=9;
+var guessedLetters=[];
+var letterToGuess=null;
+
+//1st computer to guess from random letter
+var compGuess = compChoices[Math.floor(Math.random()*compChoices.length)];
+
+//player guesses letter, presses key and releases to enter key
+document.onkeyup = function(event) {
+    var playersGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    guessesLeft --;
+    
+
+
+}    
+function playersGuessesLeft() {
+    document.querySelector("#guessesLeft").innerHtml = "Number of Player's Guesses Left: " + guessesLeft;
+};
+
+function playersGuessedLetters() {
+    document.querySelector("#letter").innerHtml = "Player's Guesses So Far: " + guessedLetters.join('');
+};
+
