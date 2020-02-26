8cde253b98292e10a1707c1dafc42f7b98f49586
Ken Dacanay <ken.p.r.dacanay@gmail.com>
2020-02-26 07:31:00 -0500

    gotten pretty far, lots of reading, no idea why i only see losses # and NOTHING ELSE smh

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,50 @@
 //these will be the letter choices for computer and user use
-var letterChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var letterChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 //this is to set/reset counters 
-var wins=0;
-var losses=0;
-var guesses=9;
-var guessesLeft=9;
-var guessedLetters=[];
-var letterToGuess=null;
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessesLeft = 9;
+var guessedLetters = [];
+var letterToGuess = null;
 
 //1st computer to guess from random letter
-var compGuess = compChoices[Math.floor(Math.random()*compChoices.length)];
+var compGuess = letterChoices[Math.floor(Math.random() * letterChoices.length)];
 
-//player guesses letter, presses key and releases to enter key
-document.onkeyup = function(event) {
-    var playersGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    guessesLeft --;
-    
-
-
-}    
 function playersGuessesLeft() {
     document.querySelector("#guessesLeft").innerHtml = "Number of Player's Guesses Left: " + guessesLeft;
 };
 
 function playersGuessedLetters() {
-    document.querySelector("#letter").innerHtml = "Player's Guesses So Far: " + guessedLetters.join('');
+    document.querySelector("#guessedLetters").innerHtml = "Player's Guesses So Far: " + guessedLetters.join('');
 };
 
+playersGuessesLeft();
+//reset game after win/9 losses
+var reset = function () {
+    guessesLeft = 9;
+    guessedLetters = [];
+    var compGuess = letterChoices[Math.floor(Math.random() * letterChoices.length)];
+}
+
+//player guesses letter, presses key and releases to enter key
+document.onkeyup = function (event) {
+    var playersGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    guessesLeft--;
+    guessedLetters.push(playersGuess);
+    playersGuessesLeft();
+    playersGuessedLetters();
+
+    if (playersGuess === compGuess) {
+        wins++;
+        document.querySelector("#wins").innerHTML = "Wins: " + wins;
+        reset();
+    } else if (guessesLeft === 0) {
+        losses++;
+        document.querySelector("#losses").innerHTML = "Losses: " + losses;
+        reset();
+    }
+
+
+};
