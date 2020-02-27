f10a6630b5b6ea13fe69c402b3cbb1ef553d06a1
Ken Dacanay <ken.p.r.dacanay@gmail.com>
2020-02-27 09:36:19 -0500

    found out on previous class to not use jquery, so that screwed up all the reading /researching i was doing (which is blindly typing things I see on stackoverflow) restarted game.js, cleaned up index.html to make it easier to manage and link to (i.e., deleted individual id tags to <p>)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,60 @@
 //these will be the letter choices for computer and user use
 var letterChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-//this is to set/reset counters 
+//counters 
 var wins = 0;
 var losses = 0;
-var guesses = 9;
-var guessesLeft = 9;
+var guessesLeft = 10;
 var guessedLetters = [];
-var letterToGuess = null;
 
-//1st computer to guess from random letter
-var compGuess = letterChoices[Math.floor(Math.random() * letterChoices.length)];
+console.log(letterChoices);
 
-function playersGuessesLeft() {
-    document.querySelector("#guessesLeft").innerHtml = "Number of Player's Guesses Left: " + guessesLeft;
-};
+document.onkeyup= function(event) {
 
-function playersGuessedLetters() {
-    document.querySelector("#guessedLetters").innerHtml = "Player's Guesses So Far: " + guessedLetters.join('');
-};
-
-playersGuessesLeft();
-//reset game after win/9 losses
-var reset = function () {
-    guessesLeft = 9;
-    guessedLetters = [];
-    var compGuess = letterChoices[Math.floor(Math.random() * letterChoices.length)];
-}
-
-//player guesses letter, presses key and releases to enter key
-document.onkeyup = function (event) {
-    var playersGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    guessesLeft--;
-    guessedLetters.push(playersGuess);
-    playersGuessesLeft();
-    playersGuessedLetters();
-
-    if (playersGuess === compGuess) {
-        wins++;
-        document.querySelector("#wins").innerHTML = "Wins: " + wins;
-        reset();
-    } else if (guessesLeft === 0) {
-        losses++;
-        document.querySelector("#losses").innerHTML = "Losses: " + losses;
-        reset();
+    console.log(event);
+    //determines which key was pressed.
+    var userGuess = event.key;
+    // randomly chooses a choice from the options array. This is the Computer's guess.
+    function cpuGuess(letterChoices) {
+         return letterChoices[Math.floor(Math.random() * letterChoices.length)];
     }
 
-
-};
+        //Pushes userGuess to wongGuesses array.
+        var addGuess = function() {
+            guessedLetters.push(
+                userGuess
+                )
+            };	
+        
+            //Resets the game.
+            var resetGame = function() {
+                    guessesLeft = 10;
+                    guessedLetters = [];
+                    cpuGuess();
+            };
+        
+            //Determines winning and losing and what happens
+            if (userGuess === cpuGuess) {
+                alert("You got it! You're out of this world!")
+                wins++;
+                resetGame(); 
+            } else {
+                guessesLeft--;
+                addGuess();
+                if (guessesLeft === 0) {
+                    alert("Disappointing. Try again and prove me wrong.");
+                    losses++;
+                    resetGame();
+                }
+            };
+        
+            //Incorporating HTML
+            var html =
+              "<p>Wins: " + wins + "</p>" +
+              "<p>Losses: " + losses + "</p>" +
+              "<p>Guesses Left: " + guessesLeft + "</p>" +
+              "<p>Player Guesses So Far: " + guessedLetters + "</p>";
+        
+            document.querySelector("#gameplay").innerHTML = html;
+        
+        };
