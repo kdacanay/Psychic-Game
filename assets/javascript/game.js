3be6f451b5ef27fe2a3b97ade45c862ad3d7f7cd
Ken Dacanay <ken.p.r.dacanay@gmail.com>
2020-02-29 20:37:04 -0500

    cleaned up code, still need to figure out preventing user from pressing other keys other than letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,48 @@
-//Assignment Rules:
-//Computer randomly picks a letter from array, user has to guess which letter the cpu chose.
-
-//this is the array of letter choices for computer and user to pick from
-let letterChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-//counters displayed on html, will reset when guesses left reach 0, or guessed correct letter
+var letters = "abcdefghijklmnopqrstuvwxyz".split("");
 var wins = 0;
 var losses = 0;
-var guessesLeft = 10;
-var guessedLetters = [];
+var guesses = 9;
+var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+var userGuessSoFar = [];
+var winPoints = document.getElementById("wins");
+var guessesLeft = document.getElementById("guessLeft");
+var losePoints = document.getElementById("losses");
+var guessSoFar = document.getElementById("guess-so-far");
 
-//used to see what letter/key the user types   
-document.onkeyup= function(event) {
-//computer randomly picks letter from letterChoices array to be matched to user guess
-function cpuGuess(letterChoices) {
-    return letterChoices[Math.floor(Math.random() * letterChoices.length)];
+function updateDisplays() {
+  winPoints.textContent = wins;
+  guessesLeft.textContent = guesses;
+  losePoints.textContent = losses;
 }
-var userGuess = event.key;
 
-        //Pushes userGuess to wongGuesses array.
+document.onkeyup = function(event) {
 
-        
-            //Resets the game.
-            var reset = function() {
-                    guessesLeft = 10;
-                    guessedLetters = [];
-                    cpuGuess();
-            };
-//if user guesses incorrectly, this takes wrong letter and "pushes" letter to guessedLetters
-var addGuess = function() {
-    guessedLetters.push(
-    userGuess)
-};	
-            //if else statements to control flow of program, alerts user if letter guessed correctly, or alerts user if loss
-            if (userGuess === cpuGuess) {
-                alert("Can you really read my mind?? Prove it again!")
-                wins++;
-                reset(); 
-            } else {
-            guessesLeft--;
-            addGuess();
-            if (guessesLeft === 0) {
-            alert("I knew you weren't psychic.  It's okay.  Try again!")
-                losses++;
-                reset();
-                }
-            };
-        
-            //this tracks stats and updates the html
-            var html =
-              "<p>Wins: " + wins + "</p>" +
-              "<p>Losses: " + losses + "</p>" +
-              "<p>Guesses Left: " + guessesLeft + "</p>" +
-              "<p>Player Guesses So Far: " + guessedLetters + "</p>";
-        
-            document.querySelector("#gameplay").innerHTML = html;
-        
-        };
+  var userGuess = event.key;
+  userGuessSoFar.push(userGuess);
+  guessSoFar.textContent = userGuessSoFar;
+
+  if (userGuess == computerGuess) {
+
+    wins++;
+    guesses = 9
+    userGuessSoFar = [];
+    computerGuess = letters[Math.floor(Math.random() * letters.length)];
+
+  } else {
+
+    guesses--;
+
+    if (guesses == 0) {
+      losses++;
+      userGuessSoFar = [];
+      guesses = 9;
+    }
+
+  }
+
+  updateDisplays();
+
+}
+
+
+updateDisplays();
